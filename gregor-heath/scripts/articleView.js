454fff515690e3a22ee2a9e21f531e66c03910ba
Gregor Richardson <gregor.richardson@gmail.com>
2017-11-30 09:51:54 -0800

    mostly done with TODOs in articleview

--- gregor-heath/scripts/articleView.js
+++ gregor-heath/scripts/articleView.js
@@ -38,13 +38,17 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
-      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
-      
+      // DONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      $(this).hide();
+      let location = $(this).val();
+      $('option[data-author]="+' + location + '"').show();
+
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
-      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-
+      // DO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      $(this).show();
+      $('.template').hide();
     }
     $('#category-filter').val('');
   });
@@ -55,11 +59,28 @@ articleView.handleCategoryFilter = function() {
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
 
+      $(this).hide();
+      let location = $(this).val();
+      $('option[data-category]="+' + location + '"').show();
+
+    } else {
+      $(this).show();
+      $('.template').hide();
+    }
+    $('#category-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
+  $('.main-nav').on('change', function() {
+    $('.tab').hide();
+    let location = $(this).val();
+    $('option[data-content]="' + location + '"').show();
+  });
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
