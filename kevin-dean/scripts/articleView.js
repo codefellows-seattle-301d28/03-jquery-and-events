2e11afd10ab948769a82dd176e00f011666b8b1c
Kevin MIller <kevin@driftabout.com>
2017-11-30 12:25:53 -0800

    refactored read more to work with show less

--- kevin-dean/scripts/articleView.js
+++ kevin-dean/scripts/articleView.js
@@ -89,11 +89,13 @@ articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
   $('.read-on').on('click', function(e) {
-    console.log($(this).parent());
     e.preventDefault();
-    $(this).parent().find('*:nth-of-type(n+2)').fadeIn(350);
+    let snip = $(this);
+    let toggleText = 'Read on &rarr;'
+    if (snip.text().indexOf('Read') > -1 ) toggleText = 'Show Less &rarr;';
+    snip.html(toggleText);
+    $(this).parent().find('*:nth-of-type(n+2)').fadeToggle(350);
   });
-
   // DONE: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
