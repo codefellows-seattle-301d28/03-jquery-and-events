7e34d8e65145528003f8a087f8e6008bc86b9b3f
Dean <dean.murphy90@gmail.com>
2017-11-30 10:28:18 -0800

    created category filter function

--- kevin-dean/scripts/articleView.js
+++ kevin-dean/scripts/articleView.js
@@ -54,7 +54,16 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn(350);
+    } else {
+      $('article').not('.template').show();
+    }
+    $('#author-filter').val('');
+  });
+  // DONE: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
@@ -82,4 +91,5 @@ articleView.setTeasers = function() {
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
 })
