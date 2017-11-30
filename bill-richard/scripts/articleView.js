ab3faaaefacdd381edf810b132b06ecdc512a140
billodell <billodell5@gmail.com>
2017-11-30 09:44:32 -0800

    normalize css added

--- bill-richard/scripts/articleView.js
+++ bill-richard/scripts/articleView.js
@@ -40,7 +40,9 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      let $authorSelection = this.authorName;
+      $('#articles').hide();
+      $('#articles[data-author="' + $authorSelection + '"]').show()
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
 
@@ -76,5 +78,5 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
 })
