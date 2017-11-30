7f19aec1344e8627c1b64f838d64ef14a8cbd950
Mitchell M <mitchellm1618@gmail.com>
2017-11-30 11:30:57 -0800

    completed all non-stretch TODOs in all js files

--- george-mitchell/scripts/articleView.js
+++ george-mitchell/scripts/articleView.js
@@ -71,7 +71,7 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   $('.main-nav').on('click', '.tab', function() {
     // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
     $('.tab-content').hide();
@@ -89,21 +89,22 @@ articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide();
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
-  $('article').on('click', 'a.read-on)', function(event) {
+  $('article').on('click', 'a.read-on', event, function() {
     event.preventDefault();
-    if ($(this).text() === 'Read on &rarr;') {
+    console.log($(this).text());
+    if ($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn(750);
 
     }
-
-  }
+  });
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
-  articleView.setTeasers();
+  articleView.setTeasers()
 })
