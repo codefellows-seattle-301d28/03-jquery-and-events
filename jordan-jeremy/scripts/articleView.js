99f0348baaa504ff5fc9b31466220ccc6af12de0
Jeremy <jpjazzy@hotmail.com>
2017-11-30 11:25:55 -0800

    fixed template from showing up

--- jordan-jeremy/scripts/articleView.js
+++ jordan-jeremy/scripts/articleView.js
@@ -40,15 +40,13 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
       // DONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
-<<<<<<< HEAD
-      $('article').fade
-=======
-      $('article').hide();
->>>>>>> 111dc15f828d64abe2fcabc9f69099500f948033
+      $('article').fadeOut();
+
       $(`article[data-author='${this.value}']`).fadeIn();
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('article').fadeIn();
+      $('.template').hide();
 
     }
     $('#category-filter').val('');
@@ -70,6 +68,7 @@ articleView.handleCategoryFilter = function() {
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('article').fadeIn();
+      $('.template').hide();
 
     }
     $('#author-filter').val('');
